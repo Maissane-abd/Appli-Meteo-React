bf7180ebac1542d740c40840e90f2ffb6a387415
Maïssane Abdallah <abd.maissane@gmail.com>
2024-03-20 02:45:56 +0100

    contenu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import loader from "./assets/loader.svg"
+
 function App() {
  
   return (
 
       <main>
-      App Meteo
+      <div className="loader-container">
+        <img src={loader} alt="loading icon" />
+      </div>
+      <p className="city-name">Paris</p>
+      <p className="country-name">France</p>
+      <p className="temperature">17°</p>
+      <div className="info-icon-container">
+        <img src="/icons/01d.svg" alt="weather icon" />
+      </div>
       </main>
   
   );
